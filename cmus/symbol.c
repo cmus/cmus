5f4425e67658dfba6854b1cd101bdf296c581818
Timo Hirvonen <tihirvon@ee.oulu.fi>
2005-06-26 21:12:40 +0300

    Add symbol.[ch]

---
+++ cmus/symbol.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2005 Timo Hirvonen
+ */
+
+#include <symbol.h>
+
+#include <dlfcn.h>
+#include <stdio.h>
+
+int get_symbol(void *handle, const char *name, const char *filename, void **symp, int null_is_valid)
+{
+	dlerror();
+	*symp = dlsym(handle, name);
+	if (*symp == NULL && !null_is_valid) {
+		fprintf(stderr, "%s: symbol %s not found\n", filename, name);
+		return 0;
+	}
+	return 1;
+}
---
+++ cmus/symbol.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2005 Timo Hirvonen
+ */
+
+#ifndef _SYMBOL_H
+#define _SYMBOL_H
+
+extern int get_symbol(void *handle, const char *name, const char *filename, void **symp, int null_is_valid);
+
+#endif
