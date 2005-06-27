2200f1ece631d80d74493585ded76ff81527ab1c
Timo Hirvonen <tihirvon@ee.oulu.fi>
2005-06-27 14:58:07 +0300

    Add `make release'
    `make release' creates tarball using git. Better than `make dist' because dist
    depends on clobber.

--- Dir.mk
+++ Dir.mk
@@ -1,3 +1,27 @@
+release	:= $(PACKAGE)-$(VERSION)
+tmpdir	:= /tmp
+tarball	:= $(DISTDIR)/$(release).tar.bz2
+
+release: extra
+	@dir=$(tmpdir)/$(release); \
+	if [[ -e $$dir ]]; \
+	then \
+		echo "$$dir exists" >&2; \
+		exit 1; \
+	fi; \
+	if [[ -e $(tarball) ]]; \
+	then \
+		echo -n "\`$(tarball)' already exists. overwrite? [n] "; \
+		read key; \
+		case $$key in y|Y) ;; *) exit 0; ;; esac; \
+	fi; \
+	echo "   DIST   $(tarball)"; \
+	cg-export $$dir || exit 1; \
+	cd $(tmpdir) || exit 1; \
+	cp $(top_srcdir)/doc/cmus.html $(release)/doc || exit 1; \
+	tar -c $(release) | bzip2 -9 > $(tarball) || rm -f $(tarball); \
+	rm -rf $(release)
+
 install-data:
 	$(SRCDIR_INSTALL) --fmode=0755 $(pkgdatadir)/example example/cmus-status-display
 
