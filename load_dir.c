e767ee8bfb853a627d8ec3eb716decf1b62d50b9
Timo Hirvonen <tihirvon@gmail.com>
2006-06-30 17:01:09 +0300

    Add directory reading functions
    dir_open() wraps opendir()
    dir_close() wraps closedir()
    dir_read() wraps readdir() and stat()
    
    Also add struct ptr_array which contains array of pointers, number of
    them and number of allocated pointers.
    
    ptr_array_add() adds a pointer to ptr_array. Array is realloced when
    needed.
    
    ptr_array_sort() is used to sort ptr_array using qsort() and user
    provided comparator.

--- load_dir.c
+++ load_dir.c
@@ -66,3 +66,68 @@ int load_dir(const char *dirname, char ***names,
 	*names = ptrs;
 	return count;
 }
+
+int dir_open(struct directory *dir, const char *name)
+{
+	int len = strlen(name);
+
+	if (len >= sizeof(dir->path) - 2) {
+		errno = ENAMETOOLONG;
+		return -1;
+	}
+
+	dir->d = opendir(name);
+	if (!dir->d)
+		return -1;
+
+	memcpy(dir->path, name, len);
+	dir->path[len++] = '/';
+	dir->path[len] = 0;
+	dir->len = len;
+	return 0;
+}
+
+void dir_close(struct directory *dir)
+{
+	closedir(dir->d);
+}
+
+const char *dir_read(struct directory *dir, struct stat *st)
+{
+	DIR *d = dir->d;
+	int len = dir->len;
+	char *full = dir->path;
+	struct dirent *de;
+
+	while ((de = readdir(d))) {
+		const char *name = de->d_name;
+		int nlen = strlen(name);
+
+		/* just ignore too long paths
+		 * + 2 -> space for \0 or / and \0
+		 */
+		if (len + nlen + 2 >= sizeof(dir->path))
+			continue;
+
+		memcpy(full + len, name, nlen + 1);
+		if (stat(full, st))
+			continue;
+
+		return full + len;
+	}
+	return NULL;
+}
+
+void ptr_array_add(struct ptr_array *array, void *ptr)
+{
+	void **ptrs = ptrs = array->ptrs;
+	int alloc = array->alloc;
+
+	if (alloc == array->count) {
+		alloc = alloc * 3 / 2 + 16;
+		ptrs = xrealloc(ptrs, alloc * sizeof(void *));
+		array->ptrs = ptrs;
+		array->alloc = alloc;
+	}
+	ptrs[array->count++] = ptr;
+}
--- load_dir.h
+++ load_dir.h
@@ -6,9 +6,47 @@
 #define _LOAD_DIR_H
 
 #include <sys/stat.h>
+#include <stdlib.h>
+#include <dirent.h>
 
 int load_dir(const char *dirname, char ***names,
 		int (*filter)(const char *name, const struct stat *s),
 		int (*compare)(const void *a, const void *b));
+
+struct directory {
+	DIR *d;
+	int len;
+	char path[1024];
+};
+
+int dir_open(struct directory *dir, const char *name);
+void dir_close(struct directory *dir);
+const char *dir_read(struct directory *dir, struct stat *st);
+
+
+struct ptr_array {
+	/* allocated with malloc(). contains pointers */
+	void *ptrs;
+	int alloc;
+	int count;
+};
+
+/* ptr_array.ptrs is either char ** or struct dir_entry ** */
+struct dir_entry {
+	mode_t mode;
+	char name[0];
+};
+
+#define PTR_ARRAY(name) struct ptr_array name = { NULL, 0, 0 }
+
+void ptr_array_add(struct ptr_array *array, void *ptr);
+
+static inline void ptr_array_sort(struct ptr_array *array,
+		int (*cmp)(const void *a, const void *b))
+{
+	int count = array->count;
+	if (count)
+		qsort(array->ptrs, count, sizeof(void *), cmp);
+}
 
 #endif
