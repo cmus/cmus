de1c4bd24cd45e78fd27c1679625894408240a28
Timo Hirvonen <tihirvon@gmail.com>
2006-07-18 23:27:43 +0300

    Add path_absolute_cwd(path, cwd)
    path_absolute_cwd(path, cwd) works like path_absolute(path) but takes
    the "current" working directory as a parameter.  This is needed for
    making symbolic link targets absolute.

--- path.c
+++ path.c
@@ -1,24 +1,5 @@
-/* 
- * Copyright 2005 Timo Hirvonen
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
- * 02111-1307, USA.
- */
-
-#include <path.h>
-#include <xmalloc.h>
+#include "path.h"
+#include "xmalloc.h"
 
 #include <stdlib.h>
 #include <unistd.h>
@@ -89,7 +70,7 @@ void path_strip(char *str)
 		str[i - 1] = 0;
 }
 
-char *path_absolute(const char *src)
+char *path_absolute_cwd(const char *src, const char *cwd)
 {
 	char *str;
 
@@ -97,12 +78,9 @@ char *path_absolute(const char *src)
 		/* already absolute */
 		str = xstrdup(src);
 	} else {
-		char cwd[1024];
 		int src_len;
 		int cwd_len;
 
-		if (getcwd(cwd, sizeof(cwd)) == NULL)
-			return NULL;
 		src_len = strlen(src);
 		cwd_len = strlen(cwd);
 		str = xnew(char, cwd_len + 1 + src_len + 1);
@@ -112,4 +90,15 @@ char *path_absolute(const char *src)
 	}
 	path_strip(str);
 	return str;
+}
+
+char *path_absolute(const char *src)
+{
+	char cwd[1024];
+
+	if (!getcwd(cwd, sizeof(cwd))) {
+		cwd[0] = '/';
+		cwd[1] = 0;
+	}
+	return path_absolute_cwd(src, cwd);
 }
