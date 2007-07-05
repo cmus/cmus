2684e08c37265d54c130b81790a0256879527698
David Thiel <lx@redundancy.redundancy.org>
2007-07-05 21:16:45 +0300

    Status script for pidgin

---
+++ contrib/cmus-updatepidgin.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python
+
+import dbus
+import sys
+
+args = {}
+
+for n in range(1, len(sys.argv) - 1, 2):
+	args[sys.argv[n]] = sys.argv[n + 1]
+
+print args
+
+bus = dbus.SessionBus()
+
+obj = bus.get_object("im.pidgin.purple.PurpleService", "/im/pidgin/purple/PurpleObject")
+pidgin = dbus.Interface(obj, "im.pidgin.purple.PurpleInterface")
+
+current = pidgin.PurpleSavedstatusGetCurrent()
+status_type = pidgin.PurpleSavedstatusGetType(current)
+saved = pidgin.PurpleSavedstatusNew("", status_type)
+pidgin.PurpleSavedstatusSetMessage(saved, "♪ %s - %s" % (args["artist"], args["title"]))
+pidgin.PurpleSavedstatusActivate(saved)
+
+
