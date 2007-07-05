4673fc0f350a60824e518f5c87b0b6b85ef75ec6
Joshua Kwan <joshk@triplehelix.org>
2007-07-05 21:10:20 +0300

    Status script for Gaim

---
+++ contrib/cmus-updategaim.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import dbus
+import sys
+
+args = {}
+
+for n in range(1, len(sys.argv) - 1, 2):
+	args[sys.argv[n]] = sys.argv[n + 1]
+
+obj = dbus.SessionBus().get_object("net.sf.gaim.GaimService", "/net/sf/gaim/GaimObject")
+gaim = dbus.Interface(obj, "net.sf.gaim.GaimInterface")
+
+current = gaim.GaimSavedstatusGetCurrent()
+status_type = gaim.GaimSavedstatusGetType(current)
+saved = gaim.GaimSavedstatusNew("", status_type)
+gaim.GaimSavedstatusSetMessage(saved, "♪ %s - %s" % (args["artist"], args["title"]))
+gaim.GaimSavedstatusActivate(saved)
+
