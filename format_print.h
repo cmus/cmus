fd20450b7144b3b77ea7c5923722e92385e988d0
Johannes Weißl <jargon@molb.org>
2011-03-10 12:47:54 +0100

    format_print.h: avoid code duplication
    Better if something has to be changed (i.e. add float/double).

--- format_print.h
+++ format_print.h
@@ -18,16 +18,15 @@ struct format_option {
 /* gcc < 4.6 and icc < 12.0 can't properly initialize anonymous unions */
 #if (defined(__GNUC__) && defined(__GNUC_MINOR__) && (__GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 6))) || \
 	(defined(__INTEL_COMPILER) && __INTEL_COMPILER < 1200)
-#define DEF_FO_STR(c)	{ .type = FO_STR,  .ch = c }
-#define DEF_FO_INT(c)	{ .type = FO_INT,  .ch = c }
-#define DEF_FO_TIME(c)	{ .type = FO_TIME, .ch = c }
-#define DEF_FO_END	{ .type = 0,       .ch = 0 }
+#define UNION_INIT(f, v) { .f = v }
 #else
-#define DEF_FO_STR(c)	{ .fo_str  = NULL, .type = FO_STR,  .ch = c }
-#define DEF_FO_INT(c)	{ .fo_int  = 0   , .type = FO_INT,  .ch = c }
-#define DEF_FO_TIME(c)	{ .fo_time = 0   , .type = FO_TIME, .ch = c }
-#define DEF_FO_END	{ .fo_str  = NULL, .type = 0,       .ch = 0 }
+#define UNION_INIT(f, v) .f = v
 #endif
+
+#define DEF_FO_STR(c)	{ UNION_INIT(fo_str,  NULL), .type = FO_STR,  .ch = c }
+#define DEF_FO_INT(c)	{ UNION_INIT(fo_int,  0),    .type = FO_INT,  .ch = c }
+#define DEF_FO_TIME(c)	{ UNION_INIT(fo_time, 0),    .type = FO_TIME, .ch = c }
+#define DEF_FO_END	{ UNION_INIT(fo_str,  NULL), .type = 0,       .ch = 0 }
 
 int format_print(char *str, int width, const char *format, const struct format_option *fopts);
 int format_valid(const char *format);
