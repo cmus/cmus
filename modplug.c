8c2643247b701da6c37a64aa1d47bcc633c770fb
Johannes Weißl <jargon@molb.org>
2011-03-31 19:20:33 +0200

    modplug: fix playback on bigendian systems
    modplug uses native byte order

--- modplug.c
+++ modplug.c
@@ -89,6 +89,9 @@ static int mod_open(struct input_plugin_data *ip_data)
 
 	ip_data->private = priv;
 	ip_data->sf = sf_bits(16) | sf_rate(44100) | sf_channels(2) | sf_signed(1);
+#ifdef WORDS_BIGENDIAN
+	ip_data->sf |= sf_bigendian(1);
+#endif
 	return 0;
 }
 
