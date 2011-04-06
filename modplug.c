1959decfe99bf94819aef53f1c9d440b81358d12
Johannes Weißl <jargon@molb.org>
2011-04-06 08:49:47 +0200

    modplug: remove compressed types
    cmus doesn't support compressed types, so remove extensions!

--- modplug.c
+++ modplug.c
@@ -216,7 +216,6 @@ const int ip_priority = 50;
 const char * const ip_extensions[] = {
 	"mod", "s3m", "xm", "it", "669", "amf", "ams", "dbm", "dmf", "dsm",
 	"far", "mdl", "med", "mtm", "okt", "ptm", "stm", "ult", "umx", "mt2",
-	"psm", "mdz", "s3z", "xmz", "itz", "mdr", "s3r", "xmr", "itr", "mdgz",
-	"s3gz", "xmgz", "itgz", NULL
+	"psm", NULL
 };
 const char * const ip_mime_types[] = { NULL };
