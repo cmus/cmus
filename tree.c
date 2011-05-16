cmus/cmus

commit f5a14e9f67fd4729465221095a5450f2cc73624c

add live filter command (:live-filter)

tree.c
@@ -421,11 +421,11 @@ void tree_init(void)
 	tree_searchable = searchable_new(NULL, &iter, &tree_search_ops);
 }
 
-struct track_info *tree_set_selected(void)
+struct tree_track *tree_get_selected(void)
 {
 	struct artist *artist;
 	struct album *album;
-	struct track_info *info;
+	struct tree_track *track;
 	struct iter sel;
 
 	if (rb_root_empty(&lib_artist_root))
@@ -437,11 +437,20 @@ struct track_info *tree_set_selected(void)
 		 * => get first album of the selected artist and first track of that album
 		 */
 		album = to_album(rb_first(&artist->album_root));
-		lib_cur_track = to_tree_track(rb_first(&album->track_root));
+		track = to_tree_track(rb_first(&album->track_root));
 	} else {
 		window_get_sel(lib_track_win, &sel);
-		lib_cur_track = iter_to_tree_track(&sel);
+		track = iter_to_tree_track(&sel);
 	}
+
+	return track;
+}
+
+struct track_info *tree_set_selected(void)
+{
+	struct track_info *info;
+
+	lib_cur_track = tree_get_selected();
 
 	lib_tree_win->changed = 1;
 	lib_track_win->changed = 1;
@@ -824,6 +833,60 @@ void tree_toggle_expand_artist(void)
 	}
 }
 
+void tree_expand_matching(const char *text)
+{
+	struct artist *artist;
+	struct rb_node *tmp1;
+	int have_track_selected = 0;
+
+	rb_for_each_entry(artist, tmp1, &lib_artist_root, tree_node) {
+		struct album *album = NULL;
+		struct rb_node *tmp2;
+		int album_matched = 0;
+
+		rb_for_each_entry(album, tmp2, &artist->album_root, tree_node) {
+			struct tree_track *tree_track = to_tree_track(rb_first(&album->track_root));
+			struct track_info *ti = ((struct simple_track *) tree_track)->info;
+			album_matched = track_info_matches_full(ti, text, TI_MATCH_ALBUM, TI_MATCH_ARTIST | TI_MATCH_ALBUMARTIST, 0);
+			if (album_matched)
+				break;
+		}
+		artist->expanded = album_matched;
+		if (!have_track_selected) {
+			struct tree_track *tree_track;
+			int track_matched = 0;
+
+			if (!album)
+				album = to_album(rb_first(&artist->album_root));
+
+			rb_for_each_entry(tree_track, tmp2, &album->track_root, tree_node) {
+				struct track_info *ti = ((struct simple_track *) tree_track)->info;
+				track_matched = track_info_matches_full(ti, text, TI_MATCH_TITLE, 0, 0);
+				if (track_matched)
+					break;
+			}
+			if (album_matched || track_matched) {
+				if (!tree_track)
+					tree_track = to_tree_track(rb_first(&album->track_root));
+				tree_sel_track(tree_track);
+				have_track_selected = 1;
+			}
+		}
+	}
+	window_changed(lib_tree_win);
+}
+
+void tree_expand_all(void)
+{
+	struct artist *artist;
+	struct rb_node *tmp;
+
+	rb_for_each_entry(artist, tmp, &lib_artist_root, tree_node) {
+		artist->expanded = 1;
+	}
+	window_changed(lib_tree_win);
+}
+
 static void remove_track(struct tree_track *track)
 {
 	if (album_selected(track->album)) {
@@ -894,10 +957,25 @@ void tree_remove_sel(void)
 
 void tree_sel_current(void)
 {
-	if (lib_cur_track) {
+	tree_sel_track(lib_cur_track);
+}
+
+void tree_sel_first(void)
+{
+	if (!rb_root_empty(&lib_artist_root)) {
+		struct artist *artist = to_artist(rb_first(&lib_artist_root));
+		struct album *album = to_album(rb_first(&artist->album_root));
+		struct tree_track *tree_track = to_tree_track(rb_first(&album->track_root));
+		tree_sel_track(tree_track);
+	}
+}
+
+void tree_sel_track(struct tree_track *t)
+{
+	if (t) {
 		struct iter iter;
 
-		CUR_ARTIST->expanded = 1;
+		t->album->artist->expanded = 1;
 
 		if (lib_cur_win == lib_tree_win) {
 			lib_cur_win = lib_track_win;
@@ -905,10 +983,10 @@ void tree_sel_current(void)
 			lib_track_win->changed = 1;
 		}
 
-		album_to_iter(CUR_ALBUM, &iter);
+		album_to_iter(t->album, &iter);
 		window_set_sel(lib_tree_win, &iter);
 
-		tree_track_to_iter(lib_cur_track, &iter);
+		tree_track_to_iter(t, &iter);
 		window_set_sel(lib_track_win, &iter);
 	}
 }

command_mode.c
@@ -589,6 +589,13 @@ static void cmd_factivate(char *arg)
 	editable_unlock();
 }
 
+static void cmd_live_filter(char *arg)
+{
+	editable_lock();
+	filters_set_live(arg);
+	editable_unlock();
+}
+
 static void cmd_filter(char *arg)
 {
 	editable_lock();
@@ -2426,6 +2433,7 @@ struct command commands[] = {
 	{ "fset",		cmd_fset,	1, 1, NULL,		  0, 0 },
 	{ "help",		cmd_help,	0, 0, NULL,		  0, 0 },
 	{ "invert",		cmd_invert,	0, 0, NULL,		  0, 0 },
+	{ "live-filter",	cmd_live_filter,0, 1, NULL,		  0, CMD_LIVE },
 	{ "load",		cmd_load,	1, 1, expand_load_save,	  0, 0 },
 	{ "lqueue",		cmd_lqueue,	0, 1, NULL,		  0, 0 },
 	{ "mark",		cmd_mark,	0, 1, NULL,		  0, 0 },
@@ -2633,6 +2641,26 @@ static void reset_tab_expansion(void)
 	arg_expand_cmd = -1;
 }
 
+static void cmdline_modified(void)
+{
+	char *cmd, *arg;
+	struct command *c;
+
+	if (!parse_command(cmdline.line, &cmd, &arg))
+		return;
+
+	c = get_command(cmd);
+	if (!c)
+		goto end;
+
+	if (c->flags & CMD_LIVE)
+		run_parsed_command(cmd, arg);
+
+end:
+	free(cmd);
+	free(arg);
+}
+
 int parse_command(const char *buf, char **cmdp, char **argp)
 {
 	int cmd_start, cmd_end, cmd_len;
@@ -2750,6 +2778,7 @@ void command_mode_ch(uchar ch)
 		break;
 	case 0x04: // ^D
 		cmdline_delete_ch();
+		cmdline_modified();
 		break;
 	case 0x05: // ^E
 		cmdline_move_end();
@@ -2776,22 +2805,27 @@ void command_mode_ch(uchar ch)
 		break;
 	case 0x0B:
 		cmdline_clear_end();
+		cmdline_modified();
 		break;
 	case 0x09:
 		tab_expand();
 		break;
 	case 0x15:
 		cmdline_backspace_to_bol();
+		cmdline_modified();
 		break;
 	case 0x17: // ^W
 		cmdline_backward_delete_word(cmdline_word_delimiters);
+		cmdline_modified();
 		break;
 	case 0x08: // ^H
 	case 127:
 		backspace();
+		cmdline_modified();
 		break;
 	default:
 		cmdline_insert_ch(ch);
+		cmdline_modified();
 	}
 	reset_history_search();
 	if (ch != 0x09)
@@ -2806,6 +2840,7 @@ void command_mode_escape(int c)
 		break;
 	case 100:
 		cmdline_delete_word(cmdline_filename_delimiters);
+		cmdline_modified();
 		break;
 	case 102:
 		cmdline_forward_word(cmdline_filename_delimiters);
@@ -2813,6 +2848,7 @@ void command_mode_escape(int c)
 	case 127:
 	case KEY_BACKSPACE:
 		cmdline_backward_delete_word(cmdline_filename_delimiters);
+		cmdline_modified();
 		break;
 	}
 	reset_history_search();
@@ -2824,9 +2860,11 @@ void command_mode_key(int key)
 	switch (key) {
 	case KEY_DC:
 		cmdline_delete_ch();
+		cmdline_modified();
 		break;
 	case KEY_BACKSPACE:
 		backspace();
+		cmdline_modified();
 		break;
 	case KEY_LEFT:
 		cmdline_move_left();

command_mode.h
@@ -22,8 +22,12 @@
 
 #include "uchar.h"
 
-/* executing command is disabled over net */
-#define CMD_UNSAFE 0x01
+enum {
+	/* executing command is disabled over net */
+	CMD_UNSAFE	= 1 << 0,
+	/* execute command after every typed/deleted character */
+	CMD_LIVE	= 1 << 1,
+};
 
 struct command {
 	const char *name;

data/rc
@@ -20,6 +20,7 @@ bind common E win-add-Q
 bind common F push filter 
 bind common G win-bottom
 bind common I echo {}
+bind common L push live-filter 
 bind common M toggle play_library
 bind common N search-prev
 bind common P win-mv-before

editable.c
@@ -272,6 +272,18 @@ void editable_clear(struct editable *e)
 	}
 }
 
+void editable_remove_matching_tracks(struct editable *e,
+		int (*cb)(void *data, struct track_info *ti), void *data)
+{
+	struct list_head *item, *tmp;
+
+	list_for_each_safe(item, tmp, &e->head) {
+		struct simple_track *t = to_simple_track(item);
+		if (cb(data, t->info))
+			editable_remove_track(e, t);
+	}
+}
+
 void editable_mark(struct editable *e, const char *filter)
 {
 	struct expr *expr = NULL;

editable.h
@@ -37,6 +37,8 @@ void editable_toggle_mark(struct editable *e);
 void editable_move_after(struct editable *e);
 void editable_move_before(struct editable *e);
 void editable_clear(struct editable *e);
+void editable_remove_matching_tracks(struct editable *e,
+		int (*cb)(void *data, struct track_info *ti), void *data);
 void editable_mark(struct editable *e, const char *filter);
 void editable_unmark(struct editable *e);
 void editable_invert_marks(struct editable *e);

expr.c
@@ -685,7 +685,7 @@ error_exit:
 	return NULL;
 }
 
-static int is_short_filter(const char *str)
+int expr_is_short(const char *str)
 {
 	int i;
 	for (i = 0; str[i]; i++) {
@@ -720,7 +720,7 @@ struct expr *expr_parse(const char *str)
 		goto out;
 	}
 
-	if (is_short_filter(str)) {
+	if (expr_is_short(str)) {
 		str = long_str = expand_short_expr(str);
 		if (!str)
 			goto out;
@@ -798,6 +798,28 @@ int expr_check_leaves(struct expr **exprp, const char *(*get_filter)(const char 
 
 	/* this sets parents left pointer */
 	*exprp = e;
+	return 0;
+}
+
+unsigned int expr_get_match_type(struct expr *expr)
+{
+	const char *key;
+
+	if (expr->left) {
+		unsigned int left = expr_get_match_type(expr->left);
+		if (expr->type == EXPR_AND || expr->type == EXPR_OR)
+			return left | expr_get_match_type(expr->right);
+		return left;
+	}
+
+	key = expr->key;
+	if (strcmp(key, "artist") == 0)
+		return TI_MATCH_ARTIST;
+	if (strcmp(key, "album") == 0 || strcmp(key, "discnumber") == 0)
+		return TI_MATCH_ALBUM;
+	if (strcmp(key, "title") == 0 || strcmp(key, "tracknumber") == 0)
+		return TI_MATCH_TITLE;
+
 	return 0;
 }
 

expr.h
@@ -54,5 +54,8 @@ int expr_check_leaves(struct expr **exprp, const char *(*get_filter)(const char 
 int expr_eval(struct expr *expr, struct track_info *ti);
 void expr_free(struct expr *expr);
 const char *expr_error(void);
+int expr_is_short(const char *str);
+
+unsigned int expr_get_match_type(struct expr *expr);
 
 #endif

commit 170e2ccbeda6fb71fa88fd0a451b07fae6e851da

fix segfault when hitting win-activate on empty tree
This bug was introduced in commit
f5a14e9: add live filter command (:live-filter)

Reported-by: Viktor Skalák <viktor.skalak@gmail.com>

tree.c
@@ -476,6 +476,8 @@ struct track_info *tree_set_selected(void)
 	struct track_info *info;
 
 	lib_cur_track = tree_get_selected();
+	if (!lib_cur_track)
+		return NULL;
 
 	lib_tree_win->changed = 1;
 	lib_track_win->changed = 1;