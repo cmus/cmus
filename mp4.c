078d93651b66a0199e2895269387593f96f0ef9e
Johannes Weißl <jargon@molb.org>
2011-05-30 01:05:10 +0200

    mp4: fix compile error
    MP4_MPEG4_AAC_HE_AUDIO_TYPE is not defined in old versions of mp4v2:
    It is defined in 1.2, but not in 0.9.8.6.
    
    Reported-by: Vladimir Todorov <inspell.87@gmail.com>

--- mp4.c
+++ mp4.c
@@ -537,7 +537,9 @@ static const char *object_type_to_str(uint8_t obj_type)
 	case MP4_MPEG4_AAC_LC_AUDIO_TYPE:	return "LC";
 	case MP4_MPEG4_AAC_SSR_AUDIO_TYPE:	return "SSR";
 	case MP4_MPEG4_AAC_LTP_AUDIO_TYPE:	return "LTP";
+#ifdef MP4_MPEG4_AAC_HE_AUDIO_TYPE
 	case MP4_MPEG4_AAC_HE_AUDIO_TYPE:	return "HE";
+#endif
 	case MP4_MPEG4_AAC_SCALABLE_AUDIO_TYPE:	return "Scalable";
 	}
 	return NULL;
