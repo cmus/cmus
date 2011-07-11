e98772131094fbe28f78af8376e2ee56dc0fd3c9
Philipp Schafft <lion@lion.leolix.org>
2011-07-11 21:30:45 +0400

    Update RoarAudio output plugin
    1. Use roar_libroar_set_server() to set server (This may be needed
       for bad NAT firewalls)
    2. Use non-blocking connect. Also changes the prio to -3 to be in
       front of pulse plugin (feel free to adjust pulse and roarplugin
       to -1 and -2).
    
    Signed-off-by: Gregory Petrosyan <gregory.petrosyan@gmail.com>

--- roar.c
+++ roar.c
@@ -1,6 +1,6 @@
 /*
  * Copyright 2008-2011 Various Authors
- * Copyright 2010 Philipp 'ph3-der-loewe' Schafft
+ * Copyright 2010-2011 Philipp 'ph3-der-loewe' Schafft
  * Copyright 2006 Johannes Weißl
  *
  * This program is free software; you can redistribute it and/or
@@ -30,6 +30,7 @@
 // so we use 2^(16-1)-1 here.
 #define MIXER_BASE_VOLUME 32767
 
+static struct roar_connection con;
 static roar_vs_t *vss = NULL;
 static int err;
 static sample_format_t format;
@@ -40,7 +41,7 @@ static char *role = NULL;
 
 static inline void _err_to_errno(void)
 {
-	roar_errno = err;
+	roar_err_set(err);
 	roar_err_to_errno();
 }
 
@@ -109,33 +110,63 @@ static int _set_role(void)
 
 static int op_roar_open(sample_format_t sf, const channel_position_t *channel_map)
 {
-	int codec = -1;
+	struct roar_audio_info info;
 	int ret;
+
+	memset(&info, 0, sizeof(info));
 
 	ROAR_DBG("op_roar_open(*) = ?");
 
 	format = sf;
 
+	info.rate = sf_get_rate(sf);
+	info.channels = sf_get_channels(sf);
+	info.bits = sf_get_bits(sf);
+
 	if (sf_get_bigendian(sf)) {
 		if (sf_get_signed(sf)) {
-			codec = ROAR_CODEC_PCM_S_BE;
+			info.codec = ROAR_CODEC_PCM_S_BE;
 		} else {
-			codec = ROAR_CODEC_PCM_U_BE;
+			info.codec = ROAR_CODEC_PCM_U_BE;
 		}
 	} else {
 		if (sf_get_signed(sf)) {
-			codec = ROAR_CODEC_PCM_S_LE;
+			info.codec = ROAR_CODEC_PCM_S_LE;
 		} else {
-			codec = ROAR_CODEC_PCM_U_LE;
+			info.codec = ROAR_CODEC_PCM_U_LE;
 		}
 	}
 
 	ROAR_DBG("op_roar_open(*) = ?");
 
-	vss = roar_vs_new_playback(host, "C* Music Player (cmus)", sf_get_rate(sf), sf_get_channels(sf), codec, sf_get_bits(sf), &err);
+	if ( roar_libroar_set_server(host) == -1 ) {
+		ROAR_DBG("op_roar_open(*) = ?");
 
+		roar_err_to_errno();
+		return -OP_ERROR_ERRNO;
+	}
+
+	if ( roar_simple_connect2(&con, NULL, "C* Music Player (cmus)", ROAR_ENUM_FLAG_NONBLOCK, 0) == -1 ) {
+		ROAR_DBG("op_roar_open(*) = ?");
+
+		roar_err_to_errno();
+		return -OP_ERROR_ERRNO;
+	}
+
+	vss = roar_vs_new_from_con(&con, &err);
 	if (vss == NULL) {
 		ROAR_DBG("op_roar_open(*) = ?");
+
+		roar_disconnect(&con);
+
+		_err_to_errno();
+		return -OP_ERROR_ERRNO;
+	}
+
+	if ( roar_vs_stream(vss, &info, ROAR_DIR_PLAY, &err) == -1 ) {
+		ROAR_DBG("op_roar_open(*) = ?");
+
+		roar_disconnect(&con);
 
 		_err_to_errno();
 		return -OP_ERROR_ERRNO;
@@ -145,6 +176,7 @@ static int op_roar_open(sample_format_t sf, const channel_position_t *channel_ma
 
 	if (roar_vs_buffer(vss, 2048*8, &err) == -1) {
 		roar_vs_close(vss, ROAR_VS_TRUE, NULL);
+		roar_disconnect(&con);
 		_err_to_errno();
 		return -OP_ERROR_ERRNO;
 	}
@@ -154,6 +186,7 @@ static int op_roar_open(sample_format_t sf, const channel_position_t *channel_ma
 	ret = _set_role();
 	if (ret != 0) {
 		roar_vs_close(vss, ROAR_VS_TRUE, NULL);
+		roar_disconnect(&con);
 		_err_to_errno();
 		return ret;
 	}
@@ -170,6 +203,7 @@ static int op_roar_open(sample_format_t sf, const channel_position_t *channel_ma
 static int op_roar_close(void)
 {
 	roar_vs_close(vss, ROAR_VS_FALSE, &err);
+	roar_disconnect(&con);
 	return 0;
 }
 
@@ -347,4 +381,4 @@ const char * const op_mixer_options[] = {
 	NULL
 };
 
-const int op_priority = -1;
+const int op_priority = -3;
