cmus/cmus

commit a9ad5ee494221b5ba4b59527e4c3535527a23eac

add reverse sort order

track_info.c
@@ -214,6 +214,28 @@ int track_info_cmp(const struct track_info *a, const struct track_info *b, const
 		case SORT_BITRATE:
 			res = getentry(a, key, long) - getentry(b, key, long);
 			break;
+		case REV_SORT_TRACKNUMBER:
+		case REV_SORT_DISCNUMBER:
+		case REV_SORT_DATE:
+		case REV_SORT_ORIGINALDATE:
+			res = getentry(b, key, int) - getentry(a, key, int);
+			break;
+		case REV_SORT_FILEMTIME:
+			res = b->mtime - a->mtime;
+			break;
+		case REV_SORT_FILENAME:
+			/* NOTE: filenames are not necessarily UTF-8 */
+			res = strcoll(b->filename, a->filename);
+			break;
+		case REV_SORT_RG_TRACK_GAIN:
+		case REV_SORT_RG_TRACK_PEAK:
+		case REV_SORT_RG_ALBUM_GAIN:
+		case REV_SORT_RG_ALBUM_PEAK:
+			res = doublecmp0(getentry(b, key, double), getentry(a, key, double));
+			break;
+		case REV_SORT_BITRATE:
+			res = getentry(b, key, long) - getentry(a, key, long);
+			break;
 		default:
 			av = getentry(a, key, const char *);
 			bv = getentry(b, key, const char *);

track_info.h
@@ -66,27 +66,47 @@ struct track_info {
 
 typedef size_t sort_key_t;
 
-#define SORT_ARTIST        offsetof(struct track_info, collkey_artist)
-#define SORT_ALBUM         offsetof(struct track_info, collkey_album)
-#define SORT_TITLE         offsetof(struct track_info, collkey_title)
-#define SORT_TRACKNUMBER   offsetof(struct track_info, tracknumber)
-#define SORT_DISCNUMBER    offsetof(struct track_info, discnumber)
-#define SORT_DATE          offsetof(struct track_info, date)
-#define SORT_ORIGINALDATE  offsetof(struct track_info, originaldate)
-#define SORT_RG_TRACK_GAIN offsetof(struct track_info, rg_track_gain)
-#define SORT_RG_TRACK_PEAK offsetof(struct track_info, rg_track_peak)
-#define SORT_RG_ALBUM_GAIN offsetof(struct track_info, rg_album_gain)
-#define SORT_RG_ALBUM_PEAK offsetof(struct track_info, rg_album_peak)
-#define SORT_GENRE         offsetof(struct track_info, collkey_genre)
-#define SORT_COMMENT       offsetof(struct track_info, collkey_comment)
-#define SORT_ALBUMARTIST   offsetof(struct track_info, collkey_albumartist)
-#define SORT_FILENAME      offsetof(struct track_info, filename)
-#define SORT_FILEMTIME     offsetof(struct track_info, mtime)
-#define SORT_BITRATE       offsetof(struct track_info, bitrate)
-#define SORT_CODEC         offsetof(struct track_info, codec)
-#define SORT_CODEC_PROFILE offsetof(struct track_info, codec_profile)
-#define SORT_MEDIA         offsetof(struct track_info, media)
-#define SORT_INVALID       ((sort_key_t) (-1))
+#define SORT_ARTIST        	offsetof(struct track_info, collkey_artist)
+#define SORT_ALBUM         	offsetof(struct track_info, collkey_album)
+#define SORT_TITLE         	offsetof(struct track_info, collkey_title)
+#define SORT_TRACKNUMBER   	offsetof(struct track_info, tracknumber)
+#define SORT_DISCNUMBER    	offsetof(struct track_info, discnumber)
+#define SORT_DATE          	offsetof(struct track_info, date)
+#define SORT_ORIGINALDATE  	offsetof(struct track_info, originaldate)
+#define SORT_RG_TRACK_GAIN 	offsetof(struct track_info, rg_track_gain)
+#define SORT_RG_TRACK_PEAK 	offsetof(struct track_info, rg_track_peak)
+#define SORT_RG_ALBUM_GAIN 	offsetof(struct track_info, rg_album_gain)
+#define SORT_RG_ALBUM_PEAK 	offsetof(struct track_info, rg_album_peak)
+#define SORT_GENRE         	offsetof(struct track_info, collkey_genre)
+#define SORT_COMMENT       	offsetof(struct track_info, collkey_comment)
+#define SORT_ALBUMARTIST   	offsetof(struct track_info, collkey_albumartist)
+#define SORT_FILENAME      	offsetof(struct track_info, filename)
+#define SORT_FILEMTIME     	offsetof(struct track_info, mtime)
+#define SORT_BITRATE       	offsetof(struct track_info, bitrate)
+#define SORT_CODEC         	offsetof(struct track_info, codec)
+#define SORT_CODEC_PROFILE 	offsetof(struct track_info, codec_profile)
+#define SORT_MEDIA		offsetof(struct track_info, media)
+#define REV_SORT_ARTIST		-offsetof(struct track_info, collkey_artist)
+#define REV_SORT_ALBUM          -offsetof(struct track_info, collkey_album)
+#define REV_SORT_TITLE          -offsetof(struct track_info, collkey_title)
+#define REV_SORT_TRACKNUMBER    -offsetof(struct track_info, tracknumber)
+#define REV_SORT_DISCNUMBER     -offsetof(struct track_info, discnumber)
+#define REV_SORT_DATE           -offsetof(struct track_info, date)
+#define REV_SORT_ORIGINALDATE   -offsetof(struct track_info, originaldate)
+#define REV_SORT_RG_TRACK_GAIN  -offsetof(struct track_info, rg_track_gain)
+#define REV_SORT_RG_TRACK_PEAK  -offsetof(struct track_info, rg_track_peak)
+#define REV_SORT_RG_ALBUM_GAIN  -offsetof(struct track_info, rg_album_gain)
+#define REV_SORT_RG_ALBUM_PEAK  -offsetof(struct track_info, rg_album_peak)
+#define REV_SORT_GENRE          -offsetof(struct track_info, collkey_genre)
+#define REV_SORT_COMMENT        -offsetof(struct track_info, collkey_comment)
+#define REV_SORT_ALBUMARTIST    -offsetof(struct track_info, collkey_albumartist)
+#define REV_SORT_FILENAME       -offsetof(struct track_info, filename)
+#define REV_SORT_FILEMTIME      -offsetof(struct track_info, mtime)
+#define REV_SORT_BITRATE        -offsetof(struct track_info, bitrate)
+#define REV_SORT_CODEC          -offsetof(struct track_info, codec)
+#define REV_SORT_CODEC_PROFILE  -offsetof(struct track_info, codec_profile)
+#define REV_SORT_MEDIA          -offsetof(struct track_info, media)
+#define SORT_INVALID            ((sort_key_t) (-1))
 
 #define TI_MATCH_ARTIST       (1 << 0)
 #define TI_MATCH_ALBUM        (1 << 1)

options.c
@@ -284,6 +284,26 @@ static const struct {
 	{ "codec",		SORT_CODEC		},
 	{ "codec_profile",	SORT_CODEC_PROFILE	},
 	{ "media",		SORT_MEDIA		},
+	{ "-artist",		REV_SORT_ARTIST		},
+	{ "-album",		REV_SORT_ALBUM		},
+	{ "-title",		REV_SORT_TITLE		},
+	{ "-tracknumber",	REV_SORT_TRACKNUMBER	},
+	{ "-discnumber",	REV_SORT_DISCNUMBER	},
+	{ "-date",		REV_SORT_DATE		},
+	{ "-originaldate",	REV_SORT_ORIGINALDATE	},
+	{ "-genre",		REV_SORT_GENRE		},
+	{ "-comment",		REV_SORT_COMMENT	},
+	{ "-albumartist",	REV_SORT_ALBUMARTIST	},
+	{ "-filename",		REV_SORT_FILENAME	},
+	{ "-filemtime",		REV_SORT_FILEMTIME	},
+	{ "-rg_track_gain",	REV_SORT_RG_TRACK_GAIN	},
+	{ "-rg_track_peak",	REV_SORT_RG_TRACK_PEAK	},
+	{ "-rg_album_gain",	REV_SORT_RG_ALBUM_GAIN	},
+	{ "-rg_album_peak",	REV_SORT_RG_ALBUM_PEAK	},
+	{ "-bitrate",		REV_SORT_BITRATE	},
+	{ "-codec",		REV_SORT_CODEC		},
+	{ "-codec_profile",	REV_SORT_CODEC_PROFILE	},
+	{ "-media",		REV_SORT_MEDIA		},
 	{ NULL,                 SORT_INVALID            }
 };
 

commit 371e0607b7a12003d69ab4e848d1c7b137a0dcef

Fix crashes caused by a9ad5ee494221b5ba4b59527e4c3535527a23eac
For some reason, I've merged referenced commit without actually testing it.
Sorry.

Signed-off-by: Gregory Petrosyan <gregory.petrosyan@gmail.com>

track_info.c
@@ -185,11 +185,17 @@ static int doublecmp0(double a, double b)
 /* this function gets called *alot*, it must be very fast */
 int track_info_cmp(const struct track_info *a, const struct track_info *b, const sort_key_t *keys)
 {
-	int i, res = 0;
+	int i, rev = 0, res = 0;
 
 	for (i = 0; keys[i] != SORT_INVALID; i++) {
 		sort_key_t key = keys[i];
 		const char *av, *bv;
+
+		rev = 0;
+		if (key >= REV_SORT__START) {
+			rev = 1;
+			key -= REV_SORT__START;
+		}
 
 		switch (key) {
 		case SORT_TRACKNUMBER:
@@ -214,28 +220,6 @@ int track_info_cmp(const struct track_info *a, const struct track_info *b, const
 		case SORT_BITRATE:
 			res = getentry(a, key, long) - getentry(b, key, long);
 			break;
-		case REV_SORT_TRACKNUMBER:
-		case REV_SORT_DISCNUMBER:
-		case REV_SORT_DATE:
-		case REV_SORT_ORIGINALDATE:
-			res = getentry(b, key, int) - getentry(a, key, int);
-			break;
-		case REV_SORT_FILEMTIME:
-			res = b->mtime - a->mtime;
-			break;
-		case REV_SORT_FILENAME:
-			/* NOTE: filenames are not necessarily UTF-8 */
-			res = strcoll(b->filename, a->filename);
-			break;
-		case REV_SORT_RG_TRACK_GAIN:
-		case REV_SORT_RG_TRACK_PEAK:
-		case REV_SORT_RG_ALBUM_GAIN:
-		case REV_SORT_RG_ALBUM_PEAK:
-			res = doublecmp0(getentry(b, key, double), getentry(a, key, double));
-			break;
-		case REV_SORT_BITRATE:
-			res = getentry(b, key, long) - getentry(a, key, long);
-			break;
 		default:
 			av = getentry(a, key, const char *);
 			bv = getentry(b, key, const char *);
@@ -246,5 +230,5 @@ int track_info_cmp(const struct track_info *a, const struct track_info *b, const
 		if (res)
 			break;
 	}
-	return res;
+	return rev ? -res : res;
 }

track_info.h
@@ -66,6 +66,7 @@ struct track_info {
 
 typedef size_t sort_key_t;
 
+#define SORT_INVALID            ((sort_key_t) (-1))
 #define SORT_ARTIST        	offsetof(struct track_info, collkey_artist)
 #define SORT_ALBUM         	offsetof(struct track_info, collkey_album)
 #define SORT_TITLE         	offsetof(struct track_info, collkey_title)
@@ -86,27 +87,27 @@ typedef size_t sort_key_t;
 #define SORT_CODEC         	offsetof(struct track_info, codec)
 #define SORT_CODEC_PROFILE 	offsetof(struct track_info, codec_profile)
 #define SORT_MEDIA		offsetof(struct track_info, media)
-#define REV_SORT_ARTIST		-offsetof(struct track_info, collkey_artist)
-#define REV_SORT_ALBUM          -offsetof(struct track_info, collkey_album)
-#define REV_SORT_TITLE          -offsetof(struct track_info, collkey_title)
-#define REV_SORT_TRACKNUMBER    -offsetof(struct track_info, tracknumber)
-#define REV_SORT_DISCNUMBER     -offsetof(struct track_info, discnumber)
-#define REV_SORT_DATE           -offsetof(struct track_info, date)
-#define REV_SORT_ORIGINALDATE   -offsetof(struct track_info, originaldate)
-#define REV_SORT_RG_TRACK_GAIN  -offsetof(struct track_info, rg_track_gain)
-#define REV_SORT_RG_TRACK_PEAK  -offsetof(struct track_info, rg_track_peak)
-#define REV_SORT_RG_ALBUM_GAIN  -offsetof(struct track_info, rg_album_gain)
-#define REV_SORT_RG_ALBUM_PEAK  -offsetof(struct track_info, rg_album_peak)
-#define REV_SORT_GENRE          -offsetof(struct track_info, collkey_genre)
-#define REV_SORT_COMMENT        -offsetof(struct track_info, collkey_comment)
-#define REV_SORT_ALBUMARTIST    -offsetof(struct track_info, collkey_albumartist)
-#define REV_SORT_FILENAME       -offsetof(struct track_info, filename)
-#define REV_SORT_FILEMTIME      -offsetof(struct track_info, mtime)
-#define REV_SORT_BITRATE        -offsetof(struct track_info, bitrate)
-#define REV_SORT_CODEC          -offsetof(struct track_info, codec)
-#define REV_SORT_CODEC_PROFILE  -offsetof(struct track_info, codec_profile)
-#define REV_SORT_MEDIA          -offsetof(struct track_info, media)
-#define SORT_INVALID            ((sort_key_t) (-1))
+#define REV_SORT__START		sizeof(struct track_info)
+#define REV_SORT_ARTIST		(REV_SORT__START + offsetof(struct track_info, collkey_artist))
+#define REV_SORT_ALBUM          (REV_SORT__START + offsetof(struct track_info, collkey_album))
+#define REV_SORT_TITLE          (REV_SORT__START + offsetof(struct track_info, collkey_title))
+#define REV_SORT_TRACKNUMBER    (REV_SORT__START + offsetof(struct track_info, tracknumber))
+#define REV_SORT_DISCNUMBER     (REV_SORT__START + offsetof(struct track_info, discnumber))
+#define REV_SORT_DATE           (REV_SORT__START + offsetof(struct track_info, date))
+#define REV_SORT_ORIGINALDATE   (REV_SORT__START + offsetof(struct track_info, originaldate))
+#define REV_SORT_RG_TRACK_GAIN  (REV_SORT__START + offsetof(struct track_info, rg_track_gain))
+#define REV_SORT_RG_TRACK_PEAK  (REV_SORT__START + offsetof(struct track_info, rg_track_peak))
+#define REV_SORT_RG_ALBUM_GAIN  (REV_SORT__START + offsetof(struct track_info, rg_album_gain))
+#define REV_SORT_RG_ALBUM_PEAK  (REV_SORT__START + offsetof(struct track_info, rg_album_peak))
+#define REV_SORT_GENRE          (REV_SORT__START + offsetof(struct track_info, collkey_genre))
+#define REV_SORT_COMMENT        (REV_SORT__START + offsetof(struct track_info, collkey_comment))
+#define REV_SORT_ALBUMARTIST    (REV_SORT__START + offsetof(struct track_info, collkey_albumartist))
+#define REV_SORT_FILENAME       (REV_SORT__START + offsetof(struct track_info, filename))
+#define REV_SORT_FILEMTIME      (REV_SORT__START + offsetof(struct track_info, mtime))
+#define REV_SORT_BITRATE        (REV_SORT__START + offsetof(struct track_info, bitrate))
+#define REV_SORT_CODEC          (REV_SORT__START + offsetof(struct track_info, codec))
+#define REV_SORT_CODEC_PROFILE  (REV_SORT__START + offsetof(struct track_info, codec_profile))
+#define REV_SORT_MEDIA          (REV_SORT__START + offsetof(struct track_info, media))
 
 #define TI_MATCH_ARTIST       (1 << 0)
 #define TI_MATCH_ALBUM        (1 << 1)