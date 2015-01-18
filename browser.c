8017965fa2261a9db58f227e8c34af10c81b9a3b
Jasper Olbrich <jasper.olbrich@students.uni-marburg.de>
2015-01-18 17:36:46 +0100

    Continue to 'cd ..' if parent directory not found.

--- browser.c
+++ browser.c
@@ -333,6 +333,12 @@ void browser_up(void)
 	pos = xstrdup(ptr);
 
 	if (browser_load(new)) {
+		if (errno == ENOENT) {
+			browser_dir = new;
+			free(pos);
+			browser_up();
+			return;
+		}
 		error_msg("could not open directory '%s': %s\n", new, strerror(errno));
 		free(new);
 		free(pos);
