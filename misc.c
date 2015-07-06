8ab4ab5b9f4fc04a8e1446be69167932e366ad25
Gregory Petrosyan <gregory.petrosyan@gmail.com>
2015-07-06 12:07:14 +0300

    misc_init: make sure ~/.config directory exists
    Fix several memory leaks while we are at it.
    
    Fixes #305

--- misc.c
+++ misc.c
@@ -184,7 +184,7 @@ static char *get_non_empty_env(const char *name)
 
 int misc_init(void)
 {
-	const char *xdg_runtime_dir = get_non_empty_env("XDG_RUNTIME_DIR");
+	char *xdg_runtime_dir = get_non_empty_env("XDG_RUNTIME_DIR");
 
 	home_dir = get_non_empty_env("HOME");
 	if (home_dir == NULL)
@@ -199,7 +199,7 @@ int misc_init(void)
 
 	cmus_config_dir = get_non_empty_env("CMUS_HOME");
 	if (cmus_config_dir == NULL) {
-		const char *cmus_home = xstrjoin(home_dir, "/.cmus");
+		char *cmus_home = xstrjoin(home_dir, "/.cmus");
 		int cmus_home_exists = dir_exists(cmus_home);
 
 		if (cmus_home_exists == 1) {
@@ -207,14 +207,18 @@ int misc_init(void)
 		} else if (cmus_home_exists == -1) {
 			die_errno("error: opening `%s'", cmus_home);
 		} else {
-			const char *xdg_config_home = get_non_empty_env("XDG_CONFIG_HOME");
-
+			char *xdg_config_home = get_non_empty_env("XDG_CONFIG_HOME");
 			if (xdg_config_home == NULL) {
-				cmus_config_dir = xstrjoin(home_dir, "/.config/cmus");
-			} else {
-				cmus_config_dir = xstrjoin(xdg_config_home, "/cmus");
+				xdg_config_home = xstrjoin(home_dir, "/.config");
 			}
+
+			make_dir(xdg_config_home);
+			cmus_config_dir = xstrjoin(xdg_config_home, "/cmus");
+
+			free(xdg_config_home);
 		}
+
+		free(cmus_home);
 	}
 	make_dir(cmus_config_dir);
 
@@ -226,6 +230,8 @@ int misc_init(void)
 			cmus_socket_path = xstrjoin(xdg_runtime_dir, "/cmus-socket");
 		}
 	}
+
+	free(xdg_runtime_dir);
 	return 0;
 }
 
