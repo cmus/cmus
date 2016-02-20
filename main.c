56f1439dbddd1651a83b2b7d584f1552aedabf2d
yne <yne@users.noreply.github.com>
2016-02-20 11:52:14 +0100

    Fix a NULL dereference on file_url_absolute call made by main

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2008-2013 Various Authors
  * Copyright 2005-2006 Timo Hirvonen
@@ -390,7 +391,7 @@ int main(int argc, char *argv[])
 		send_cmd("player-play\n");
 	if (flags[FLAG_PAUSE])
 		send_cmd("player-pause\n");
-	if (flags[FLAG_FILE])
+	if (flags[FLAG_FILE] && play_file)
 		send_cmd("player-play %s\n", file_url_absolute(play_file));
 	if (volume)
 		send_cmd("vol %s\n", volume);
