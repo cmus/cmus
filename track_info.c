2aa606ff2a3fa5d80e8f02eda9c24b0c8ee3e9ae
Julian Orth <ju.orth@gmail.com>
2016-07-09 20:16:36 +0200

    Move sort key parsing to track_info.c

--- track_info.c
+++ track_info.c
@@ -25,6 +25,7 @@
 #include "utils.h"
 #include "debug.h"
 #include "path.h"
+#include "ui_curses.h"
 
 #include <string.h>
 #include <math.h>
@@ -244,4 +245,136 @@ int track_info_cmp(const struct track_info *a, const struct track_info *b, const
 			break;
 	}
 	return rev ? -res : res;
+}
+
+static const struct {
+	const char *str;
+	sort_key_t key;
+} sort_key_map[] = {
+	{ "artist",		SORT_ARTIST		},
+	{ "album",		SORT_ALBUM		},
+	{ "title",		SORT_TITLE		},
+	{ "play_count",		SORT_PLAY_COUNT		},
+	{ "tracknumber",	SORT_TRACKNUMBER	},
+	{ "discnumber",		SORT_DISCNUMBER		},
+	{ "date",		SORT_DATE		},
+	{ "originaldate",	SORT_ORIGINALDATE	},
+	{ "genre",		SORT_GENRE		},
+	{ "comment",		SORT_COMMENT		},
+	{ "albumartist",	SORT_ALBUMARTIST	},
+	{ "filename",		SORT_FILENAME		},
+	{ "filemtime",		SORT_FILEMTIME		},
+	{ "rg_track_gain",	SORT_RG_TRACK_GAIN	},
+	{ "rg_track_peak",	SORT_RG_TRACK_PEAK	},
+	{ "rg_album_gain",	SORT_RG_ALBUM_GAIN	},
+	{ "rg_album_peak",	SORT_RG_ALBUM_PEAK	},
+	{ "bitrate",		SORT_BITRATE		},
+	{ "codec",		SORT_CODEC		},
+	{ "codec_profile",	SORT_CODEC_PROFILE	},
+	{ "media",		SORT_MEDIA		},
+	{ "bpm",		SORT_BPM		},
+	{ "-artist",		REV_SORT_ARTIST		},
+	{ "-album",		REV_SORT_ALBUM		},
+	{ "-title",		REV_SORT_TITLE		},
+	{ "-play_count", 	REV_SORT_PLAY_COUNT	},
+	{ "-tracknumber",	REV_SORT_TRACKNUMBER	},
+	{ "-discnumber",	REV_SORT_DISCNUMBER	},
+	{ "-date",		REV_SORT_DATE		},
+	{ "-originaldate",	REV_SORT_ORIGINALDATE	},
+	{ "-genre",		REV_SORT_GENRE		},
+	{ "-comment",		REV_SORT_COMMENT	},
+	{ "-albumartist",	REV_SORT_ALBUMARTIST	},
+	{ "-filename",		REV_SORT_FILENAME	},
+	{ "-filemtime",		REV_SORT_FILEMTIME	},
+	{ "-rg_track_gain",	REV_SORT_RG_TRACK_GAIN	},
+	{ "-rg_track_peak",	REV_SORT_RG_TRACK_PEAK	},
+	{ "-rg_album_gain",	REV_SORT_RG_ALBUM_GAIN	},
+	{ "-rg_album_peak",	REV_SORT_RG_ALBUM_PEAK	},
+	{ "-bitrate",		REV_SORT_BITRATE	},
+	{ "-codec",		REV_SORT_CODEC		},
+	{ "-codec_profile",	REV_SORT_CODEC_PROFILE	},
+	{ "-media",		REV_SORT_MEDIA		},
+	{ "-bpm",		REV_SORT_BPM		},
+	{ NULL,                 SORT_INVALID            }
+};
+
+sort_key_t *parse_sort_keys(const char *value)
+{
+	sort_key_t *keys;
+	const char *s, *e;
+	int size = 4;
+	int pos = 0;
+
+	keys = xnew(sort_key_t, size);
+
+	s = value;
+	while (1) {
+		char buf[32];
+		int i, len;
+
+		while (*s == ' ')
+			s++;
+
+		e = s;
+		while (*e && *e != ' ')
+			e++;
+
+		len = e - s;
+		if (len == 0)
+			break;
+		if (len > 31)
+			len = 31;
+
+		memcpy(buf, s, len);
+		buf[len] = 0;
+		s = e;
+
+		for (i = 0; ; i++) {
+			if (sort_key_map[i].str == NULL) {
+				error_msg("invalid sort key '%s'", buf);
+				free(keys);
+				return NULL;
+			}
+
+			if (strcmp(buf, sort_key_map[i].str) == 0)
+				break;
+		}
+		if (pos == size - 1) {
+			size *= 2;
+			keys = xrenew(sort_key_t, keys, size);
+		}
+		keys[pos++] = sort_key_map[i].key;
+	}
+	keys[pos] = SORT_INVALID;
+	return keys;
+}
+
+const char *sort_key_to_str(sort_key_t key)
+{
+	int i;
+	for (i = 0; sort_key_map[i].str; i++) {
+		if (sort_key_map[i].key == key)
+			return sort_key_map[i].str;
+	}
+	return NULL;
+}
+
+void sort_keys_to_str(const sort_key_t *keys, char *buf, size_t bufsize)
+{
+	int i, pos = 0;
+
+	for (i = 0; keys[i] != SORT_INVALID; i++) {
+		const char *key = sort_key_to_str(keys[i]);
+		int len = strlen(key);
+
+		if ((int)bufsize - pos - len - 2 < 0)
+			break;
+
+		memcpy(buf + pos, key, len);
+		pos += len;
+		buf[pos++] = ' ';
+	}
+	if (pos > 0)
+		pos--;
+	buf[pos] = 0;
 }
