cmus/cmus

commit a4540236be7895fca797fe3aac9cb1c3a9c61089

Make sure our headers take precedence over system headers
For example, a conflict is possible with our own id3.h and the one from
id3lib, making the build fail on OpenBSD.

Thus, $(topdir) needs to be included at the earliest possible stage in
the compiler flags, so that our source directory takes priority in
finding header files.

Makefile
@@ -16,7 +16,9 @@ all: main plugins man
 -include config.mk
 include scripts/lib.mk
 
-CFLAGS += -D_FILE_OFFSET_BITS=64 -I"${topdir}"
+# $(topdir) must be included at the earliest possible stage, to avoid
+# conflicts with system headers.
+CFLAGS := -I"$(topdir)" -D_FILE_OFFSET_BITS=64 $(CFLAGS)
 
 FFMPEG_CFLAGS += $(shell pkg-config --cflags libswresample)
 FFMPEG_LIBS += $(shell pkg-config --libs libswresample)

commit 219894c5154044aa0ca76c0abfb1bfa9a2f6fee3

Fix depfile generation
a4540236 broke lazy expansion of -dep-$@ in CFLAGS by performing an :=
assignment of CFLAGS in the Makefile.

Makefile
@@ -16,9 +16,7 @@ all: main plugins man
 -include config.mk
 include scripts/lib.mk
 
-# $(topdir) must be included at the earliest possible stage, to avoid
-# conflicts with system headers.
-CFLAGS := -I"$(topdir)" -D_FILE_OFFSET_BITS=64 $(CFLAGS)
+CFLAGS += -D_FILE_OFFSET_BITS=64
 
 FFMPEG_CFLAGS += $(shell pkg-config --cflags libswresample)
 FFMPEG_LIBS += $(shell pkg-config --libs libswresample)

scripts/checks.sh
@@ -172,7 +172,7 @@ check_cc()
 	check_program $CC || return 1
 
 	cc_cxx_common
-	CFLAGS="$CFLAGS $common_cf"
+	CFLAGS="$CFLAGS -I$(pwd) $common_cf"
 	LDFLAGS="$LDFLAGS $common_lf"
 
 	makefile_vars CC LD CFLAGS LDFLAGS