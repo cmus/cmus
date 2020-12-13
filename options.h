2aa28a1b165134d457e5ee5a5f2a3ff4a896e155
edclements <ed.clements@gmail.com>
2020-12-13 19:01:04 +0000

    Stop after queue (resolves #696) (#1006)
    Adds a new option to prevent playback continuing onto the whole library
    once the end of the queue is reached. Defaults to false. Enable with
    `:set stop_after_queue=false`

--- options.h
+++ options.h
@@ -148,6 +148,7 @@ extern int mouse;
 extern int mpris;
 extern int time_show_leading_zero;
 extern int start_view;
+extern int stop_after_queue;
 
 extern const char * const aaa_mode_names[];
 extern const char * const view_names[NR_VIEWS + 1];
