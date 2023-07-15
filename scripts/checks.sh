4123b54bad3d8874205aad7f1885191c8e93343c
Vladislav Grudinin <vlad.grudinin@gmail.com>
2023-07-15 15:14:44 +0400

    Fixed function detection. check_function failed to compile due to clang -Wint-conversion warning became error.

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -495,7 +495,7 @@ check_function()
 	__func="$1"
 	shift
 	msg_checking "for function $__func"
-	if try_compile_link "char $__func(); int main(int argc, char *argv[]) { return $__func; }" "$@"
+	if try_compile_link "char $__func(); int main(int argc, char *argv[]) { return ((int*)(&$__func))[argc]; }" "$@"
 	then
 		msg_result yes
 		return 0
