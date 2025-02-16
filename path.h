d38672fc53534503b6958c749606708515148a7d
ihy123 <aladinandreyy@gmail.com>
2025-02-16 20:54:14 +0300

    cue: handle adding a directory with cue files

--- path.h
+++ path.h
@@ -21,6 +21,7 @@
 
 const char *get_extension(const char *filename);
 const char *path_basename(const char *path);
+char *path_dirname(const char *path);
 void path_strip(char *str);
 char *path_absolute_cwd(const char *src, const char *cwd);
 char *path_absolute(const char *src);
